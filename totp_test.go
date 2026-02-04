6d978d9a741fd98d6163d269e6c0b8729d5b71e0
rohitaryal <erucix@gmail.com>
2026-02-05 05:23:09 +0530

    added simple test cases totp

---
+++ totp_test.go
@@ -0,0 +1,35 @@
+package totp
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGenerateSecret(t *testing.T) {
+	secret, err := GenerateSecret()
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	if strings.TrimSpace(secret) == "" {
+		t.FailNow()
+	}
+
+	t.Log("Generated secret: ", secret)
+}
+
+func TestGenerateTotp(t *testing.T) {
+	secret := "JBSWY3DPEHPK3PXP"
+
+	otp, err := GenerateTotp(secret, 0)
+	if err != nil {
+		t.FailNow()
+	}
+
+	if otp != "282760" {
+		t.FailNow()
+	}
+
+	t.Log("Generated OTP: ", otp)
+}
